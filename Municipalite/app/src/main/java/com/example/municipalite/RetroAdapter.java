3959f305b162e3ea71b0d835e3c5a5161d0cced5
ziedth <zied.hack@gmail.com>
2019-12-21 23:47:54 +0100

    add_activity_events for articles

--- Municipalite/app/src/main/java/com/example/municipalite/RetroAdapter.java
+++ Municipalite/app/src/main/java/com/example/municipalite/RetroAdapter.java
@@ -1,4 +1,85 @@
 package com.example.municipalite;
 
-public class RetroAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class RetroAdapter extends BaseAdapter{
+
+    private Context context;
+    private ArrayList<ModelListView> dataModelArrayList;
+
+    public RetroAdapter(Context context, ArrayList<ModelListView> dataModelArrayList) {
+
+        this.context = context;
+        this.dataModelArrayList = dataModelArrayList;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return getCount();
+    }
+    @Override
+    public int getItemViewType(int position) {
+
+        return position;
+    }
+    @Override
+    public int getCount() {
+        return dataModelArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dataModelArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.retro_lv, null, true);
+
+            holder.iv = (ImageView) convertView.findViewById(R.id.iv);
+            holder.tvname = (TextView) convertView.findViewById(R.id.name);
+            holder.tvcountry = (TextView) convertView.findViewById(R.id.country);
+            holder.tvcity = (TextView) convertView.findViewById(R.id.city);
+            holder.tvdate = (TextView) convertView.findViewById(R.id.date);
+
+            convertView.setTag(holder);
+        }else {
+            // the getTag returns the viewHolder object set as a tag to the view
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        Picasso.get().load(dataModelArrayList.get(position).getImgURL()).into(holder.iv);
+        holder.tvname.setText(dataModelArrayList.get(position).getName());
+        holder.tvcountry.setText(dataModelArrayList.get(position).getAdress()+"  "+dataModelArrayList.get(position).getCity());
+        holder.tvdate.setText(dataModelArrayList.get(position).getDateD()+" \n"+dataModelArrayList.get(position).getHeurD()+" "+dataModelArrayList.get(position).getHeurF());
+        holder.tvcity.setText(dataModelArrayList.get(position).getDescription());
+        return convertView;
+    }
+
+    private class ViewHolder {
+
+        protected TextView tvname, tvcountry, tvcity, tvdate;
+        protected ImageView iv;
+    }
+
 }
--- Municipalite/app/src/main/java/com/example/municipalite/activity_list_events.java
+++ Municipalite/app/src/main/java/com/example/municipalite/activity_list_events.java
@@ -2,13 +2,113 @@ package com.example.municipalite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class activity_list_events extends AppCompatActivity {
+    private ListView listView;
+    private RetroAdapter retroAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_events);
+
+        Intent intent = getIntent();
+
+        listView = findViewById(R.id.lv);
+        getJSONResponse();
     }
+
+    private void getJSONResponse(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MyInterface.JSONURL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        MyInterface api = retrofit.create(MyInterface.class);
+
+        Call<String> call = api.getString();
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.i("Responsestring", response.body().toString());
+                //Toast.makeText()
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        Log.i("onSuccess", response.body().toString());
+
+                        String jsonresponse = response.body().toString();
+                        writeListView(jsonresponse);
+
+                    } else {
+                        Log.i("onEmptyResponse", "Returned empty response");//Toast.makeText(getContext(),"Nothing returned",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(activity_list_events.this,"Server invalid \n"+t.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void writeListView(String response){
+
+        try {
+            //getting the whole json object from the response
+            JSONObject obj = new JSONObject(response);
+            if(obj.optString("success").equals("true")){
+                Toast.makeText(activity_list_events.this, "Do Login Successfully!", Toast.LENGTH_SHORT)
+                        .show();
+
+                ArrayList<ModelListView> modelListViewArrayList = new ArrayList<>();
+                JSONArray dataArray  = obj.getJSONArray("data");
+
+                for (int i = 0; i < dataArray.length(); i++) {
+
+                    ModelListView modelListView = new ModelListView();
+                    JSONObject dataobj = dataArray.getJSONObject(i);
+
+                    modelListView.setImgURL(dataobj.getString("image"));
+                    modelListView.setName(dataobj.getString("title"));
+                    modelListView.setAdress(dataobj.getString("region"));
+                    modelListView.setCity(dataobj.getString("adress"));
+                    modelListView.setDateD(dataobj.getString("date_debut"));
+                    modelListView.setHeurD(dataobj.getString("Heur_debut"));
+                    modelListView.setHeurF(dataobj.getString("Heur_fin"));
+                    modelListView.setDescription(dataobj.getString("mini_description"));
+
+                    modelListViewArrayList.add(modelListView);
+
+                }
+
+                retroAdapter = new RetroAdapter(this, modelListViewArrayList);
+                listView.setAdapter(retroAdapter);
+
+            }else {
+                Toast.makeText(activity_list_events.this, obj.optString("message")+"", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }

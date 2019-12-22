03cefd119f4f26e635740d2245b6fc29bec794f3
ziedth <zied.hack@gmail.com>
2019-12-22 19:25:44 +0100

    list_view for events

--- Municipalite/app/src/main/java/com/example/municipalite/MyInterface.java
+++ Municipalite/app/src/main/java/com/example/municipalite/MyInterface.java
@@ -5,9 +5,9 @@ import retrofit2.http.GET;
 
 public interface MyInterface {
 
-    String JSONURL = "http://196.234.17.237/municipalite2/public/api/";
+    String JSONURL = "http://196.224.17.80/municipalite2/public/api/";
     //String JSONURL ="https://demonuts.com/Demonuts/JsonTest/Tennis/json_parsing.php";
-    String img = "http://196.234.17.237/municipalite2/image/";
+    String img = "http://196.224.17.80/municipalite2/image/";
 
     @GET("articles")
     Call<String> getString();
